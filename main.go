e0a36f2ff54e22e8353de2eca9715f37957bffd3
maddox-bayn <eoduh22@gmail.com>
2026-01-13 15:38:23 -0800

    update of repo in playground

--- main.go
+++ main.go
@@ -3,53 +3,37 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
+	"io"
 	"net/http"
-	"os"
-
-	"github.com/maddox-bayn/go-playground/strutils"
 )
 
-type helloResp struct {
-	Name      string `json:"name"`
-	RuneCount int    `json:"rune_count"`
-	FirstRune string `json:"first_rune,omitempty"`
-	LastRune  string `json:"last_rune,omitempty"`
+type Hello struct {
+	Message string `json:"message"`
 }
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	name := r.URL.Query().Get("name")
-	if name == "" {
-		name = "world"
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	h := Hello{Message: "hello"}
+	w.Header().Set("content-type", "application/json")
+	if err := json.NewEncoder(w).Encode(h); err != nil {
+		http.Error(w, "the sever encounted an error that could not process your request", http.StatusInternalServerError)
+		return
 	}
+}
 
-	resp := helloResp{
-		Name:      name,
-		RuneCount: strutils.RuneCount(name),
+func echo(w http.ResponseWriter, r *http.Request) {
+	// Implement echo functionality here
+	defer r.Body.Close()
+	w.Header().Set("content-type", "application/json")
+	if _, err := io.ReadAll(io.LimitReader(r.Body, 10 << 20)); err != nil {
+		http.Error(w, "ecountered problem on stream of data", http.StatusInternalServerError)
 	}
-	if name != "" {
-		resp.FirstRune = fmt.Sprintf("%q", strutils.FirstRune(name))
-		resp.LastRune = fmt.Sprintf("%q", strutils.LastRune(name))
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	_ = json.NewEncoder(w).Encode(resp)
 }
 
 func main() {
-	// If no CLI arg provided, start HTTP server.
-	if len(os.Args) < 2 {
-		http.HandleFunc("/hello", helloHandler)
-		addr := ":8080"
-		log.Printf("Starting server on %s", addr)
-		log.Fatal(http.ListenAndServe(addr, nil))
-		return
+	http.HandleFunc("/hello", HelloHandler)
+	http.HandleFunc("/echo", echo)
+	fmt.Println("starting sever on port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Printf("Failed to start server %v\n:", err)
 	}
-
-	// CLI mode: show string utilities for the provided word.
-	word := os.Args[1]
-	fmt.Printf("Word: %v\n", word)
-	fmt.Printf("RuneCount: %d\n", strutils.RuneCount(word))
-	fmt.Printf("FirstRune: %q\n", strutils.FirstRune(word))
-	fmt.Printf("LastRune: %q\n", strutils.LastRune(word))
 }
